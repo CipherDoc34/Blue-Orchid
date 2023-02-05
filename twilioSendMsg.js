c80f5d6981d9ef1ad588269bdb15c85f83947735
Keshav Aggarwal <19ka47@queensu.ca>
2023-02-04 22:21:50 -0500

    auth token file

--- twilioSendMsg.js
+++ twilioSendMsg.js
@@ -1,12 +1,16 @@
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(accountSid, authToken);
+const fs = require("fs");
+const authTokens = fs.readFileSync("./secret.json");
+const accountSid = JSON.parse(authTokens).twilioAccountSID;
+const authToken = JSON.parse(authTokens).twilioAuthToken;
 
-client.messages.create({
-    to: '+16474584963', // Text this number
-    from: '+12898142583', // From a valid Twilio number
-    body: 'Hello from Blue Orchid',
-  })
-  .then((message) => console.log(message.sid));
+///////////////////////////////////////////////////////////////
+// const client = require('twilio')(accountSid, authToken);
 
+// client.messages.create({
+//     to: '+16474584963', // Text this number
+//     from: '+12898142583', // From a valid Twilio number
+//     body: 'Hello from Blue Orchid',
+//   })
+//   .then((message) => console.log(message.sid));
+//////////////////////////////////////////////////////////////
 
