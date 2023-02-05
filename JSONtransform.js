01e3bd3beec84d64fc5850b89adc385e3cd9af63
Keshav Aggarwal <19ka47@queensu.ca>
2023-02-05 08:38:49 -0500

    get response

--- JSONtransform.js
+++ JSONtransform.js
@@ -1,35 +1,50 @@
 const { json } = require("express");
 const fs = require("fs");
 let JSONData = [];
+// fs.readFile('./intents.json', 'utf8', (err, jsonString) => {
+//     if (err) {
+//         console.log("File read failed:", err)
+//         return
+//     }
+//     JSON.parse(jsonString).forEach(element => {
+//         element.responses.forEach(i =>{
+//             let a = {};
+//              a['text'] = i;
+//              a['label'] = element.tag;
+//              JSONData.push({'text':i, 'label': element.tag});
+//         });
+        
+//     });
+//     fs.writeFileSync('./responses.json', JSON.stringify(JSONData));
+// });
+
 fs.readFile('./intents.json', 'utf8', (err, jsonString) => {
     if (err) {
         console.log("File read failed:", err)
         return
     }
     JSON.parse(jsonString).forEach(element => {
-        element.responses.forEach(i =>{
-            let a = {};
-             a['text'] = i;
-             a['label'] = element.tag;
-             JSONData.push({'text':i, 'label': element.tag});
-        });
+        let a = {};
+        a[element.tag] = element.responses;
+        JSONData.push(a);
         
     });
-    fs.writeFileSync('./responses.json', JSON.stringify(JSONData));
+    fs.writeFileSync('./responsesdifferent.json', JSON.stringify(JSONData));
 });
-fs.readFile('./intents.json', 'utf8', (err, jsonString) => {
-    if (err) {
-        console.log("File read failed:", err)
-        return
-    }
-    JSON.parse(jsonString).forEach(element => {
-        element.patterns.forEach(i =>{
-            let a = {};
-             a['text'] = i;
-             a['label'] = element.tag;
-             JSONData.push({'text':i, 'label': element.tag});
-        });
+
+// fs.readFile('./intents.json', 'utf8', (err, jsonString) => {
+//     if (err) {
+//         console.log("File read failed:", err)
+//         return
+//     }
+//     JSON.parse(jsonString).forEach(element => {
+//         element.patterns.forEach(i =>{
+//             let a = {};
+//              a['text'] = i;
+//              a['label'] = element.tag;
+//              JSONData.push({'text':i, 'label': element.tag});
+//         });
         
-    });
-    fs.writeFileSync('./inputs.json', JSON.stringify(JSONData));
-});
+//     });
+//     fs.writeFileSync('./inputs.json', JSON.stringify(JSONData));
+// });
--- twilioReceiveMsg.js
+++ twilioReceiveMsg.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const { json } = require('body-parser');
 const fs = require('fs');
 const request = require('request');
+const Understand = require('twilio/lib/rest/preview/Understand');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 var jsonParser = bodyParser.json()
@@ -12,17 +13,27 @@ var jsonParser = bodyParser.json()
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 const app = express();
-app.post('/sms', urlencodedParser, (req, res) => {
+app.post('/', urlencodedParser, (req, res) => {
+    const twiml = new MessagingResponse();
     console.log(req.body['Body']);
     request.post("http://6.tcp.ngrok.io:16417/sentimentpost", {"form": {"input": req.body['Body']}}, (err, CohereRepsonse, body)=>{
-    //fs.readFile('./responses.json', (responses)=>{
-        //const twiml = new MessagingResponse();
-        //twiml.message('This is some message from 18:39');
-        //res.writeHead(200, {'Content-Type': 'text/xml'});
-        //res.end(twiml.toString());
-        
-    //})
-    res.end(body);
+        predictionResult = JSON.parse(body).body.classifications[0].prediction;
+        console.log("Prediction: ", predictionResult);
+        const resp = fs.readFileSync('./responsesindexlist.json');
+            let allresp = JSON.parse(resp);
+            var respindex = allresp.index[predictionResult];
+            //console.log(allresp.responses[26]['suicide']);
+            var arrayLength = allresp.responses[respindex][predictionResult].length;
+            var chosenIndex = getRandomInt(arrayLength);
+            var chosenResponse = allresp.responses[respindex][predictionResult][chosenIndex];
+            //allresp.index.array.forEach(element => {
+            twiml.message(chosenResponse);
+            res.writeHead(200, {'Content-Type': 'text/xml'});
+            res.end(twiml.toString());
+        //})
+    //console.log(predictionResult);
+    console.log("Response: ",chosenResponse);
+    //res.end(chosenResponse);
 }
     )
 });
@@ -32,3 +43,8 @@ http.createServer(app).listen(1337, () => {
     console.log('listening on port 1337!');
 
 });
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
